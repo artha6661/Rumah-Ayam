d5ad86f1882118334ed34daa0090450e33743dea
artha bastanta <arthabastanta03@gmail.com>
2023-09-13 14:37:47 +0700

    First Merge

---
+++ src/main/java/com/rumahayam/arthabastanta/RumahAyamApplication.java
@@ -0,0 +1,13 @@
+package com.rumahayam.arthabastanta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RumahAyamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RumahAyamApplication.class, args);
+	}
+
+}
