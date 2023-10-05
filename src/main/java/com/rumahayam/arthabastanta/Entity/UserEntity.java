21e16e661ee046a13083dce562afe8baf28bc4df
artha bastanta <arthabastanta03@gmail.com>
2023-10-05 10:10:23 +0700

    RE-CONFIGURE USER AND ROLE

--- src/main/java/com/rumahayam/arthabastanta/Entity/UserEntity.java
+++ src/main/java/com/rumahayam/arthabastanta/Entity/UserEntity.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDate;
+
 
 @Getter
 @Setter
@@ -23,21 +23,6 @@ public class UserEntity
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable=false)
-    private String name;
-
-    @Column(nullable=false, unique=true)
-    private String email;
-
-    @Column(nullable=false)
-    private String password;
-
-    @ManyToMany(fetch = FetchType.EAGER, cascade=CascadeType.ALL)
-    @JoinTable(
-            name="users_roles",
-            joinColumns={@JoinColumn(name="USER_ID", referencedColumnName="ID")},
-            inverseJoinColumns={@JoinColumn(name="ROLE_ID", referencedColumnName="ID")})
-    private List<RoleEntity> roles = new ArrayList<>();
 
 }
 
